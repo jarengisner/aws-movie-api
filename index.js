16cefba313de7006933c64bb2fcb2db0c67dba8a
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-03-29 19:57:43 -0700

    Added a few GET requests, to return sample data in JSON, and to respond with a sample welcome message. Documentation is also now routed through the public folder

--- index.js
+++ index.js
@@ -1,2 +1,66 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+//list of my top 10 movies//
+let myFavoriteMovies = [
+  {
+    Rank: 1,
+    title: 'Interstellar',
+  },
+  {
+    rank: 2,
+    title: 'Shawshank Redemption',
+  },
+  {
+    rank: 3,
+    title: 'Everything Everywhere All at Once',
+  },
+  {
+    rank: 4,
+    title: 'Spirited Away',
+  },
+  {
+    rank: 5,
+    title: 'Get Out',
+  },
+  {
+    rank: 6,
+    title: 'Midsommar',
+  },
+  {
+    rank: 7,
+    title: 'Joker',
+  },
+  {
+    rank: 8,
+    title: 'Life Aquatica',
+  },
+  {
+    rank: 9,
+    title: 'Us',
+  },
+  {
+    rank: 10,
+    title: 'Parasite',
+  },
+];
+
+app.use(express.static('public'));
+app.use(morgan('common'));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  console.log('Something went wrong!');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(myFavoriteMovies);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my favorite movies!');
+});
+
+app.listen(8080, () => {
+  console.log('Thee app is running on 8080');
+});
