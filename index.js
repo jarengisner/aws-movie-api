d84d08accaa1efda092bca982fed8da9fbcb7152
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-04-02 16:02:22 -0700

    Edited documentation to contain details about URL endpoints, also began to add endpoints to our index.js

--- index.js
+++ index.js
@@ -49,8 +49,82 @@ let myFavoriteMovies = [
 app.use(express.static('public'));
 app.use(morgan('common'));
 
+//GET Requests//
 app.get('/movies', (req, res) => {
   res.json(myFavoriteMovies);
+});
+//Accepts a title as a request parameter, to filter movies//
+//Accepts a string//
+app.get('/movies/:title', (req, res) => {
+  res.json(
+    myFavoriteMovies.find((movie) => {
+      return movie.title === req.params.title;
+    })
+  );
+});
+//Accepts the name of an actor as a request parameter//
+//Accepts a string//
+app.get('/movies/:actors', (req, res) => {
+  res.json(
+    myFavoriteMovies.find((movie) => {
+      return movie.actors === req.params.actors;
+    })
+  );
+});
+//Accepts the name of a director as a request parameter//
+//Accepts a string//
+app.get('/movies/:directors', (req, res) => {
+  res.json(
+    myFavoriteMovies.find((movie) => {
+      return movie.directors === req.params.directors;
+    })
+  );
+});
+//Accepts a genre as a request parameter//
+//Accepts a string//
+app.get('/movies/:genre', (req, res) => {
+  res.json(
+    myFavoriteMovies.find((movie) => {
+      return movie.genre === req.params.genre;
+    })
+  );
+});
+
+app.get('/users/favorites/', (req, res) => {
+  res.send(
+    'In the future this will send a list of the current users favorites list'
+  );
+});
+
+app.get('/directors', (req, res) => {
+  res.json(leadDirectors);
+});
+
+app.get('/actors', (req, res) => {
+  res.json(leadActors);
+});
+
+app.get('/directors/:name', (req, res) => {
+  res.json(
+    leadDirectors.find((director) => {
+      return director.name === req.params.name;
+    })
+  );
+});
+
+app.get('/actors/:name', (req, res) => {
+  res.json(
+    leadActors.find((actor) => {
+      return actor.name === req.params.name;
+    })
+  );
+});
+
+//POST Requests//
+app.post('users/register', (req, res) => {
+  res.send(
+    'Will take in an email from a form in the future, and return that username post-validation, along with status code'
+  );
 });
 
 app.get('/', (req, res) => {
