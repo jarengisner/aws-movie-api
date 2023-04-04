4789b341420b3d617ac52df5ff9fee1058032a24
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-04-03 21:51:00 -0700

    Added endpoints for requests to API, and added more detail to the documentation document

--- index.js
+++ index.js
@@ -7,44 +7,84 @@ let myFavoriteMovies = [
   {
     Rank: 1,
     title: 'Interstellar',
+    directors: 'Christopher Nolan',
+    actors: 'Matthew McConaughey',
+    genre: 'sci-fi',
   },
   {
     rank: 2,
     title: 'Shawshank Redemption',
+    directors: 'Frank Darabont',
+    actors: 'Tim Robbins',
+    genre: 'drama',
   },
   {
     rank: 3,
     title: 'Everything Everywhere All at Once',
+    directors: 'Daniel Kwan',
+    actors: 'Michelle Yeoh',
+    genre: 'drama',
   },
   {
     rank: 4,
     title: 'Spirited Away',
+    directors: 'Hayao Miyazaki',
+    actors: 'Rumi Hiiragi',
+    genre: 'action',
   },
   {
     rank: 5,
     title: 'Get Out',
+    directors: 'Jordan Peele',
+    actors: 'Daniel Kaluuya',
+    genre: 'horror',
   },
   {
     rank: 6,
     title: 'Midsommar',
+    directors: 'Ari Aster',
+    actors: 'Florence Pugh',
+    genre: 'horror',
   },
   {
     rank: 7,
     title: 'Joker',
+    directors: 'Todd Phillips',
+    actors: 'Joaquin Pheonix',
+    genre: 'drama',
   },
   {
     rank: 8,
     title: 'Life Aquatica',
+    directors: 'Wes Anderson',
+    actors: 'Bill Murray',
+    genre: 'comedy',
   },
   {
     rank: 9,
     title: 'Us',
+    directors: 'Jordan Peele',
+    actors: "Lupita Nyong'o",
+    genre: 'horror',
   },
   {
     rank: 10,
     title: 'Parasite',
+    directors: 'Bong Joon-Ho',
+    actors: 'Choi Woo-Shik',
+    genre: 'horror',
   },
 ];
+
+let leadActors = [];
+let leadDirectors = [];
+
+(function createLists() {
+  myFavoriteMovies.forEach((movie) => {
+    leadActors.push(movie.actors);
+    leadDirectors.push(movie.directors);
+  });
+})();
 
 app.use(express.static('public'));
 app.use(morgan('common'));
@@ -64,7 +104,7 @@ app.get('/movies/:title', (req, res) => {
 });
 //Accepts the name of an actor as a request parameter//
 //Accepts a string//
-app.get('/movies/:actors', (req, res) => {
+app.get('/movies/:actor', (req, res) => {
   res.json(
     myFavoriteMovies.find((movie) => {
       return movie.actors === req.params.actors;
@@ -104,29 +144,46 @@ app.get('/actors', (req, res) => {
   res.json(leadActors);
 });
 
+//accepts string for name//
 app.get('/directors/:name', (req, res) => {
-  res.json(
-    leadDirectors.find((director) => {
-      return director.name === req.params.name;
-    })
-  );
+  res.send('In the future will respond with all details about an director');
 });
 
+//accepts string for name//
 app.get('/actors/:name', (req, res) => {
-  res.json(
-    leadActors.find((actor) => {
-      return actor.name === req.params.name;
-    })
-  );
+  res.send('Will in the future respond with all details about an actor');
 });
 
 //POST Requests//
-app.post('users/register', (req, res) => {
+app.post('/users/register', (req, res) => {
+  res.status(200).send('Working!');
+});
+
+app.post('/users/favorites', (req, res) => {
+  res.status(200).send('Working!');
+});
+
+//PUT Requests//
+//accepts string for users username//
+app.put('/users/:username', (req, res) => {
   res.send(
-    'Will take in an email from a form in the future, and return that username post-validation, along with status code'
+    'In future, application will accept new username through this request parameter'
   );
 });
 
+//DELETE//
+//accepts string as title of the movie//
+app.delete('/users/favorites/:title', (req, res) => {
+  res.send('When favorites are available, will be able to delte movie by name');
+});
+
+//accepts string as username of the user//
+app.delete('/users/:username', (req, res) => {
+  res.send(
+    'When users have availability to register, function will search to see if user is registered, and then delete the user.'
+  );
+});
+//Home page, in future will send you to index page//
 app.get('/', (req, res) => {
   res.send('Welcome to my favorite movies!');
 });
