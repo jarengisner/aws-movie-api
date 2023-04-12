5855f961c820c17079df936075e5735f5ebd60fa
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-04-11 20:31:31 -0700

    Added Mongoose to project, and defined schema that our db collections will follow

---
+++ models.js
@@ -0,0 +1,35 @@
+//requires mongoose, importing it's functionality into this file//
+const mongoose = require('mongoose');
+
+//sets schema for our movie data//
+//sets our key value pairs with some controls for what data type they accept//
+let movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String,
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+  },
+  Actors: [String],
+  ImageUrl: String,
+  Featured: Boolean,
+});
+//lays out the schema for our users//
+let userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'movies' }],
+});
+
+//pairs our collection name with our collection schema, telling our collection to follow this schema//
+let Movie = mongoose.model('movies', movieSchema);
+let User = mongoose.model('users', userSchema);
+//exports our movie schema and our user schema, so that we can import them in other documents//
+module.exports.Movie = Movie;
+module.exports.User = User;
