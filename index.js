a5db12ceede91687f55468b739993eb32edf052d
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-04-21 20:03:37 -0700

    Added CORS for request restrictions, and bcrypt for password hashing

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const Models = require('./models.js');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const cors = require('cors');
+app.use(cors());
+
+//If we want to restrict CORS to onlly certain domains, use this code, otherwise all is default//
+//have to change our above cors method to the below to restrict//
+/*let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));*/
+
 let auth = require('./auth.js')(app);
 const passport = require('passport');
 require('./passport.js');
