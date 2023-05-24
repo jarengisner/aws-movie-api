8c9e7f9a907fed054776ea8f8538cae36951e800
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-05-24 13:17:17 -0700

    Changes to Models

--- models.js
+++ models.js
@@ -42,6 +42,7 @@ userSchema.statics.hashPassword = (password) => {
 userSchema.methods.validatePassword = function (password) {
   //Here is where our password input is hashed when logging in and compared to the stored hashed password//
   console.log(this.Password);
+  console.log(password);
   return bcrypt.compare(password, this.Password);
 };
 /*Could this work:
